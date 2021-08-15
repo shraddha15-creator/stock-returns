082d84617f7681c3f61cb52026eb2eed8f5899ec
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-08-15 16:56:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var initialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var currentPrice = document.querySelector("#current-price");
+
+var submitBtn = document.querySelector("#submit-btn");
+var output = document.querySelector("#output");
+
+submitBtn.addEventListener("click", submitHandler)
+
+function submitHandler() {
+    var ip = Number(initialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var curr = Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip, qty, curr);
+}
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if(initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+
+        showOutput(`Hey the loss is ${loss} and the percentage is ${lossPercentage} %`);
+    }else if(current > initial) {
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
+
+        showOutput(`Hey the loss is ${profit} and the percentage is ${profitPercentage} %`);
+    }else{
+        showOutput(`There is No Profit No Loss!!`)
+    }
+}
+
+function showOutput(message) {
+    output.innerHTML = message;
+}
